303c8d9af4afa1405e7c1cec0aaf1ea39870139d
nza3552 <nza3552@rit.edu>
2020-08-12 23:19:53 -0400

    dll issue fixed. Running into a null pointer in Mesh::Draw. No idea what's causing it

--- CollisionTestbed/CollisionTestbed.cpp
+++ CollisionTestbed/CollisionTestbed.cpp
@@ -9,7 +9,9 @@ int main(void)
 	Renderer r = Renderer(1960, 1080);
 	printf("###initializing\n");
 	r.initialize();
+	printf("###adding model\n");
 	r.addModel("C:\\Users\\noahm\\source\\repos\\CollisionTestbed\\CollisionTestbed\\survival backpack\\Survival_BackPack_2.fbx");
+	printf("###running\n");
 	r.run();
 	printf("###ending\n");
 }
--- enc_temp_folder/a4449fdaf6036b27fe522f3fff28591/Mesh.cpp
+++ enc_temp_folder/a4449fdaf6036b27fe522f3fff28591/Mesh.cpp
@@ -79,7 +79,7 @@ void Mesh::draw(Shader& shader)
 
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
+    //glBindVertexArray(0);
 
     glActiveTexture(GL_TEXTURE0);
 }
