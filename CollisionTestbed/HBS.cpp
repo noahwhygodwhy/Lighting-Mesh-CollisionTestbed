4c99da5faaf97594881e95b2d6af7c31f17a8f36
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2020-11-23 16:44:04 -0500

    started working on HBS

---
+++ CollisionTestbed/HBS.cpp
@@ -0,0 +1,13 @@
+#include "HBS.hpp"
+
+void handleHits(vector<IThing*> things)
+{
+	pair<IThing*, IThing*> maybeTouching;
+	for (int i = 0; i < things.size(); i++)//for each thing
+	{
+		for (int j = i + 1; j < things.size(); j++)//for each other thing to that thing that hasn't already been done
+		{
+			for(auto hb1 : things.at(i).)
+		}
+	}
+}
---
+++ CollisionTestbed/HBS.hpp
@@ -0,0 +1,4 @@
+#include <vector>
+#include "Thing.hpp"
+
+void handleHits(vector<IThing*> things); 
