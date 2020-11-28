56072a4b13438cca2ff0cf97272a38b9fdc754bb
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2020-11-28 16:37:00 -0500

    reconfigured which function in HBS will actually make the correction, and separated 3 stages into functions

--- CollisionTestbed/HBS.cpp
+++ CollisionTestbed/HBS.cpp
@@ -24,6 +24,22 @@ bool generalArea(vec3 t1pos, vec3 t2pos, vec3 origMax, vec3 origMin, vec3 otherM
 	return true;
 }
 
+float getDistance(vec3 a, vec3 b)
+{
+	float diffx = a.x - b.x;
+	float diffy = a.y - b.y;
+	float diffz = a.z - b.z;
+
+	return sqrt((diffx * diffx) + (diffy * diffy) + (diffz * diffz));
+}
+
+float getDistance(vec2 a, vec2 b)
+{
+	float diffx = a.x - b.x;
+	float diffy = a.y - b.y;
+
+	return sqrt((diffx * diffx) + (diffy * diffy));
+}
 float getDistance(float x1, float y1, float x2, float y2)
 {
 	float diffx = x1 - x2;
@@ -32,7 +48,7 @@ float getDistance(float x1, float y1, float x2, float y2)
 
 }
 
-bool cuboidAndCylinder(CuboidHitbox* hb1, CylinderHitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction)
+bool cuboidAndCylinder(CuboidHitbox* hb1, CylinderHitbox* hb2, vec3 t1pos, vec3 t2pos, bool correct)
 {
 	vec3 circleCenter = hb2->origin;
 
@@ -57,7 +73,7 @@ bool cuboidAndCylinder(CuboidHitbox* hb1, CylinderHitbox* hb2, vec3 t1pos, vec3 
 	return false;
 }
 
-bool cuboidAndCuboid(CuboidHitbox* hb1, CuboidHitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction)
+bool cuboidAndCuboid(CuboidHitbox* hb1, CuboidHitbox* hb2, vec3 t1pos, vec3 t2pos, bool correct)
 {
 
 	if (hb1->otherCorner.x+t1pos.x < hb2->origin.x + t2pos.x) return false;
@@ -70,16 +86,16 @@ bool cuboidAndCuboid(CuboidHitbox* hb1, CuboidHitbox* hb2, vec3 t1pos, vec3 t2po
 	return true;
 }
 
-bool cuboidAndSphere(CuboidHitbox* hb1, SphereHitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction)
+bool cuboidAndSphere(CuboidHitbox* hb1, SphereHitbox* hb2, vec3 t1pos, vec3 t2pos, bool correct)
 {
-	//TODO:
+	
 }
-bool sphereAndSphere(SphereHitbox* hb1, SphereHitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction)
+bool sphereAndSphere(SphereHitbox* hb1, SphereHitbox* hb2, vec3 t1pos, vec3 t2pos, bool correct)
 {
 	 
 }
 
-bool coliding(Hitbox* hb1, Hitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction)
+bool coliding(Hitbox* hb1, Hitbox* hb2, vec3 t1pos, vec3 t2pos, bool correct)
 {
 	int hbt = (int)hb1->type & (int)hb2->type;
 
@@ -88,8 +104,8 @@ bool coliding(Hitbox* hb1, Hitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction
 		if (hbt & (int)HitboxType::SPHERE)
 		{
 			if ((int)hb1->type & (int)HitboxType::CUBOID)
-				return cuboidAndSphere((CuboidHitbox*)hb1, (SphereHitbox*)hb2, t1pos, t2pos, correction);
-			return cuboidAndSphere((CuboidHitbox*)hb2, (SphereHitbox*)hb1, t2pos, t1pos, correction);
+				return cuboidAndSphere((CuboidHitbox*)hb1, (SphereHitbox*)hb2, t1pos, t2pos, correct);
+			return cuboidAndSphere((CuboidHitbox*)hb2, (SphereHitbox*)hb1, t2pos, t1pos, correct);
 
 		}
 		if (hbt & (int)HitboxType::PLAIN)
@@ -99,8 +115,8 @@ bool coliding(Hitbox* hb1, Hitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction
 		if (hbt & (int)HitboxType::CYLINDER)
 		{
 			if ((int)hb1->type & (int)HitboxType::CUBOID)
-				return cuboidAndCylinder((CuboidHitbox*)hb1, (CylinderHitbox*)hb2, t1pos, t2pos, correction);
-			return cuboidAndCylinder((CuboidHitbox*)hb2, (CylinderHitbox*)hb1,t2pos, t1pos, correction);
+				return cuboidAndCylinder((CuboidHitbox*)hb1, (CylinderHitbox*)hb2, t1pos, t2pos, correct);
+			return cuboidAndCylinder((CuboidHitbox*)hb2, (CylinderHitbox*)hb1,t2pos, t1pos, correct);
 
 		}
 		if (hbt & (int)HitboxType::PLLLPP)
@@ -109,8 +125,12 @@ bool coliding(Hitbox* hb1, Hitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction
 		}
 		else //both cuboids
 		{
-			return cuboidAndCuboid((CuboidHitbox*)hb1, (CuboidHitbox*)hb2, t1pos, t2pos, correction);
+			return cuboidAndCuboid((CuboidHitbox*)hb1, (CuboidHitbox*)hb2, t1pos, t2pos, correct);
 		}
+	}
+	if (hbt & (int)HitboxType::SPHERE)
+	{
+		return sphereAndSphere((SphereHitbox*) hb1, (SphereHitbox*) hb2, t2pos, t2pos, correct);
 	}
 	return false;
 	//TODO: eventually this will have to include how to handle all of the types of hitboxes. Is there a better way of doing this besides each pairs?
@@ -123,20 +143,12 @@ bool coliding(Hitbox* hb1, Hitbox* hb2, vec3 t1pos, vec3 t2pos, vec3* correction
 
 static int tasdf = 0;
 
-void handleHits(vector<IThing*> things, IThing* specificThing)
+vector<IThing*> general(vector<IThing*> things, IThing* specificThing)
 {
-
-	//definetly optimize better later^tm
 	vec3 dump;
-
 	vec3 maxs = ((Thing*)specificThing)->maxs;//TODO:
 	vec3 mins = ((Thing*)specificThing)->mins;
-
 	vector<IThing*> generalAreaThings;
-
-	//printf("things:\n");
-	
-	//create a list of general area things
 	for (int i = 0; i < things.size(); i++)
 	{
 		if (things.at(i) == specificThing)
@@ -150,9 +162,11 @@ void handleHits(vector<IThing*> things, IThing* specificThing)
 			generalAreaThings.push_back(things.at(i));
 		}
 	}
-	//printf("match: %p\n", generalAreaThings.at(0));
+	return generalAreaThings;
+}
+vector<IThing*> broad(vector<IThing*> things, IThing* specificThing)
+{
 
-	//create a list of maybe touching things
 	vector<IThing*> maybeTouching;
 	for (int i = 0; i < things.size(); i++)
 	{
@@ -169,7 +183,7 @@ void handleHits(vector<IThing*> things, IThing* specificThing)
 			}
 			for (auto hb2 : ((Thing*)specificThing)->generalHitbox) //for each hitbox in the second thing
 			{
-				if (coliding(hb1, hb2, vec3(((Thing*)things.at(i))->transform[3]), vec3(((Thing*)specificThing)->transform[3]), &dump))
+				if (coliding(hb1, hb2, vec3(((Thing*)things.at(i))->transform[3]), vec3(((Thing*)specificThing)->transform[3]), false))
 				{
 					maybeTouching.push_back(things.at(i));
 					shouldBreak = true;
@@ -178,34 +192,49 @@ void handleHits(vector<IThing*> things, IThing* specificThing)
 			}
 		}
 	}
-	//printf("match: %p\n", maybeTouching.at(0));
-	//create a list of definitely touching things, and find out by how much
-	vec3 byHowMuch;
-	for (IThing* mtThing : maybeTouching)
+	return maybeTouching;
+}
+bool narrow(vector<IThing*> things, IThing* specificThing)
+{
+
+	for (IThing* mtThing : things)
 	{
 		if (mtThing == specificThing)
 		{
 			continue;
 		}
-		bool shouldBreak = false;
 		for (auto hb1 : ((Thing*)mtThing)->generalHitbox) //for each hitbox in the first thing
 		{
-			if (shouldBreak)
-			{
-				break;
-			}
 			for (auto hb2 : ((Thing*)specificThing)->generalHitbox) //for each hitbox in the second thing
 			{
-				if (coliding(hb1, hb2, vec3(((Thing*)mtThing)->transform[3]), vec3(((Thing*)specificThing)->transform[3]), &byHowMuch))
+				if (coliding(hb1, hb2, vec3(((Thing*)mtThing)->transform[3]), vec3(((Thing*)specificThing)->transform[3]), true))
 				{
 					printf("%i THEY'RE COLLIDING\n", tasdf++);
 					//TODO:make the correction
-					shouldBreak = true;
-					break;
+					return true;
 				}
 			}
 		}
 	}
+	return false;
+}
+
+void handleHits(vector<IThing*> things, IThing* specificThing)
+{
+
+	//definetly optimize better later^tm
+	
+
+	vector<IThing*> generalAreaThings = general(things, specificThing);
+
+	vector<IThing*> maybeTouching = broad(generalAreaThings, specificThing);
+
+	while (narrow(maybeTouching, specificThing))
+	{
+
+	}
+	//printf("match: %p\n", maybeTouching.at(0));
+	//create a list of definitely touching things, and find out by how much
 }
 
 
